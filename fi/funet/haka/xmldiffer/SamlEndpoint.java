d1d34aedf9e205de5e26e1b372aa2908f22c3e75
Kari Laalo <kari.laalo@csc.fi>
2015-07-15 16:40:40 +0300

    endpoint change detection

---
+++ fi/funet/haka/xmldiffer/SamlEndpoint.java
@@ -0,0 +1,41 @@
+package fi.funet.fi.haka.xmldiffer;
+
+public class SamlEndpoint  {
+	
+	private String location;
+	private String binding;
+	
+	public SamlEndpoint (String location, String binding) {
+		this.location = location;
+		this.binding = binding;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+	public String getBinding() {
+		return binding;
+	}
+	
+	@Override
+	public boolean equals(Object other) {
+		if (other == null) return false;
+		if (other == this) return true;
+		if (!(other instanceof SamlEndpoint)) return false;
+		SamlEndpoint oth = (SamlEndpoint) other;
+		if (oth.getBinding().equals(this.getBinding()) &&
+				oth.getLocation().equals(this.getLocation())) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return this.getBinding() + " | " + this.getLocation();
+	}
+	
+	
+
+}
