4a0f0794dcdcbe7d4a469619def8dac22a69bf3e
Kari Laalo <klaalo@csc.fi>
2016-11-01 16:41:17 +0200

    Rearrange

--- src/main/java/fi/csc/virtu/samlxmltooling/tools/SamlDocBuilder.java
+++ src/main/java/fi/csc/virtu/samlxmltooling/tools/SamlDocBuilder.java
@@ -1,8 +1,8 @@
 package fi.csc.virtu.samlxmltooling.tools;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.net.URLConnection;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -15,8 +15,8 @@ import org.xml.sax.SAXException;
 
 import com.github.vbauer.herald.annotation.Log;
 
-import fi.csc.virtu.samlxmltooling.diffservlet.MainConfiguration;
 import fi.csc.virtu.samlxmltooling.diffservlet.DiffTask.TaskFlavor;
+import fi.csc.virtu.samlxmltooling.diffservlet.MainConfiguration;
 
 public class SamlDocBuilder {
 	
@@ -51,18 +51,21 @@ public class SamlDocBuilder {
 		return getDoc(url);
 	}
 	
-	private Document getDoc (URL url) throws IOException, ParserConfigurationException, SAXException {
-		URLConnection conn = url.openConnection(); 
+	private DocumentBuilder getBuilder() throws ParserConfigurationException {
 		DocumentBuilderFactory fac = DocumentBuilderFactory.newInstance();
 
 		// without this, the validator will fail
         fac.setNamespaceAware(true);
 		
-		DocumentBuilder builder;
-		builder = fac.newDocumentBuilder();
-		Document doc = builder.parse(conn.getInputStream());
-		return doc;
-		
+		return fac.newDocumentBuilder();
+	}
+
+	private Document getDoc (URL url) throws IOException, ParserConfigurationException, SAXException {
+		return getBuilder().parse(url.toString());
+	}
+	
+	public Document getDoc (File file) throws SAXException, IOException, ParserConfigurationException {
+		return getBuilder().parse(file);
 	}
 
 }
