319c1e7d0d8b26b9a6f9cf322e15549b35d72a03
Kari Laalo <klaalo@csc.fi>
2017-02-22 17:01:52 +0200

    complete publish script

--- src/main/sh/publish.sh
+++ src/main/sh/publish.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-echo "$1"
-sleep 5
+targetFile=virtu-metadata-test-2.xml
+hosts=( "virtu-pubhost01.csc.fi" "virtu-pubhost02.csc.fi" )
+
+for host in "${hosts[@]}"
+do
+    echo "copying file $1 to ${targetFile} on ${host}"
+    scp -4i /opt/saml-tooling/id_rsa $1 virtu_pubuser@${host}:/var/www/ds/fed/virtu/${targetFile} 2>1
+    if [ $? -ne 0 ]; then
+        echo "ERROR copying"
+        exit 1
+    fi
+done
+
+
 echo "end"
